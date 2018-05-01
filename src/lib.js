edf7c99ffd2670a720dc57137f25371daf2381b4
heyunfeng <hyf_cloud@163.com>
2018-05-01 16:25:56 +0800

    change

--- src/lib.js
+++ src/lib.js
@@ -1,8 +1,8 @@
 // const host = 'http://localhost:3000/'
 // const host = 'http://118.24.6.178/'
 // const host = 'http://47.98.116.23/'
-// const host = 'https://heyunf.com/'
-const host = 'https://www.hzhtyl.com/'
+const host = 'https://heyunf.com/'
+// const host = 'https://www.hzhtyl.com/'
 const Url = {
     aedInfo: host + 'aedinfo',
     deleteAed: host + 'deleteAed',
